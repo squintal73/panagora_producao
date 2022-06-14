b79239ed922d64bbcf5ced015866887cd7eeb04b
squintal73 <sidnei_quintal@yahoo.com.br>
2022-06-14 03:19:03 -0300

    V.1.0

--- app/Http/Controllers/ApiAssembleiaController.php
+++ app/Http/Controllers/ApiAssembleiaController.php
@@ -51,17 +51,6 @@ class ApiAssembleiaController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -89,40 +78,6 @@ class ApiAssembleiaController extends Controller
             ['codigo_evento', '=', $eventoId],
         ])->get();
         return response()->json($assembleias);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit()
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy()
-    {
-        //
     }
 
     public function geraPdf($id)
